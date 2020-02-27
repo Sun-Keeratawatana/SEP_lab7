2aab30822c224ea634468220b405bf1b0182a55f
Sun-Keeratawatana <61090032@kmitl.ac.th>
2020-02-27 18:24:36 +0700

    MyCommitment

--- Transportation.py
+++ Transportation.py
@@ -22,24 +22,28 @@ class Walk( Transportation ):
       return 0
 
 class Taxi(Transportation):
-    def __init__(self, s_p, e_p, rate, d):
-        super().__init__(s_p, e_p, d)
-        self._rate = rate
-        
+    def __init__(self, s_p, e_p, d):
+        super().__init__(s_p, e_p, d)        
     def find_cost(self):
-        self._cost = super().getD() * self._rate
+        self._cost = self.distance * 40
         return self._cost
 
+class Train(Transportation):
+    def __init__(self, s_p, e_p, distance, station):
+        super().__init__(s_p, e_p, distance)
+        self._station = station
+        
+    def find_cost(self):
+        self._cost = self._station * 5
+        return self._cost 
 
-   
-# main program
 
 travel_cost = 0
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
+trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+         Taxi("KMITL SCB Bank","Ladkrabang Station",5),
+         Train("Ladkrabang Station","Payathai Station",40,6),
+         Taxi("Payathai Station","The British Council",3) ]
 
 for travel in trip:
    travel_cost += travel.find_cost()
